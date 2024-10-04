2e321e00387e6e461d1e75aa638de78f69c78138
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-10-05 00:01:49 +0300

    Initial Commit

---
+++ NetworkInterchangable/NetworkInterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangableApp.swift
+//  NetworkInterchangable
+//
+//  Created by Beyza Nur Tekerek on 5.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
